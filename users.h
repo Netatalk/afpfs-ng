2ae06abe3ea0a720e0a521666001e7e3dc99c279
Alex deVries <alexthepuffin@gmail.com>
2007-02-06 17:32:36 +0000

    Fix compiler warnings.
    git-svn-id: file:///tmp/tmp.jyKeACR6iO/svn-version/trunk@20 f84e74b7-95bb-4e84-9b5c-02b1165c2552

--- users.h
+++ users.h
@@ -3,9 +3,11 @@
 
 
 void user_deleteall(struct afp_server * server);
-int user_findbyhostid(struct afp_server * server, unsigned int hostid, 
-	unsigned int *serverid);
+int user_findbyhostid(struct afp_server * server, unsigned char isuid,
+	unsigned int hostid, unsigned int *serverid);
 void user_add(struct afp_server * server,
 	unsigned int hostid,unsigned int serverid);
+int user_findbyserverid(struct afp_server * server, unsigned char isuid,
+        unsigned int serverid, unsigned int *hostid);
 
 #endif
