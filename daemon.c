b53f53d0c20b035e3b6a9007eeaac91aec310278
Alex deVries <alexthepuffin@gmail.com>
2007-09-07 05:20:10 +0000

    Clearing up a comment.
    git-svn-id: file:///tmp/tmp.jyKeACR6iO/svn-version/trunk@119 f84e74b7-95bb-4e84-9b5c-02b1165c2552

--- daemon.c
+++ daemon.c
@@ -329,10 +329,6 @@ int main(int argc, char *argv[]) {
 	};
 	int new_log_method=LOG_METHOD_SYSLOG;
 	int dofork=1;
-	/* getopt_long()'s return is int; specifying the variable to contain
-	 * this return value as char depends on endian-specific behavior,
-	 * breaking utterly on big endian (i.e., PowerPC)
-	 */
 	int c;
 	int optnum;
 	int command_fd;
