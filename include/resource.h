cac0d2f808cd437395e995200a8f47a617df7a04
Alex deVries <alexthepuffin@gmail.com>
2007-11-09 04:25:40 +0000

    restructuring include files
    git-svn-id: file:///tmp/tmp.jyKeACR6iO/svn-version/trunk@208 f84e74b7-95bb-4e84-9b5c-02b1165c2552

--- include/resource.h
+++
@@ -1,7 +0,0 @@
-#ifndef __RESOURCE_H_
-#define __RESOURCE_H_
-int apple_translate(struct afp_volume * volume, char * path);
-int is_apple(const char * path);
-int is_double_apple(const char * path);
-#endif
-
