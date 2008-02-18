6c5589c844490601a1701ba187da5557293a4ada
Alex deVries <alexthepuffin@gmail.com>
2008-02-18 03:36:30 +0000

    Simplify startup a bit.
    git-svn-id: file:///tmp/tmp.jyKeACR6iO/svn-version/trunk@339 f84e74b7-95bb-4e84-9b5c-02b1165c2552

--- lib/client.c
+++ lib/client.c
@@ -4,9 +4,20 @@
 
 struct libafpclient * libafpclient = NULL;
 
+static struct libafpclient null_afpclient = {
+	.unmount_volume = NULL,
+	.log_for_client = stdout_log_for_client,
+	.forced_ending_hook = NULL,
+	.scan_extra_fds = NULL,
+	.loop_started = NULL,
+};
+
 
 void libafpclient_register(struct libafpclient * tmpclient)
 {
-	libafpclient=tmpclient;
+	if (tmpclient) 
+		libafpclient=tmpclient;
+	else 
+		libafpclient=&null_afpclient;
 }
 
