2235cc67ebb2c6de29afa7b2614c7b6a0f8d205d
Alex deVries <alexthepuffin@gmail.com>
2008-02-18 03:44:11 +0000

    closedt support.
    git-svn-id: file:///tmp/tmp.jyKeACR6iO/svn-version/trunk@346 f84e74b7-95bb-4e84-9b5c-02b1165c2552

--- lib/proto_desktop.c
+++ lib/proto_desktop.c
@@ -44,7 +44,8 @@ int afp_geticon(struct afp_volume * volume , unsigned int filecreator,
 	request_packet.length=htons(length);
 
 	return dsi_send(volume->server, (char *)&request_packet,
-		sizeof(request_packet),DSI_DEFAULT_TIMEOUT,afpGetIcon,icon);
+		sizeof(request_packet),DSI_DEFAULT_TIMEOUT,
+		afpGetIcon,(void *) icon);
 }
 
 int afp_geticon_reply(struct afp_server *server, char * buf, unsigned int size, void * other)
@@ -67,7 +68,7 @@ int afp_geticon_reply(struct afp_server *server, char * buf, unsigned int size, 
 
 
 int afp_addcomment(struct afp_volume *volume, unsigned int did, 
-	char * pathname, char * comment, uint64_t *size)
+	const char * pathname, char * comment, uint64_t *size)
 {
 	struct {
 		struct dsi_header dsi_header __attribute__((__packed__));
@@ -83,7 +84,7 @@ int afp_addcomment(struct afp_volume *volume, unsigned int did,
 	int rc;
 
 	msg=malloc(len+1);
-	bzero(msg,len+1);
+	memset(msg,0,len+1);
 	p=msg+(sizeof(*request_packet));
 	request_packet=(void *) msg;
 	dsi_setup_header(volume->server,&request_packet->dsi_header,DSI_DSICommand);
@@ -107,14 +108,14 @@ int afp_addcomment(struct afp_volume *volume, unsigned int did,
 	*size=strlen(comment);
 
 	rc=dsi_send(volume->server, (char *)msg,len,DSI_DEFAULT_TIMEOUT,
-		afpAddComment,comment);
+		afpAddComment,(void *) comment);
 	free(msg);
 	return rc;
 
 }
 
 int afp_getcomment(struct afp_volume *volume, unsigned int did, 
-	char * pathname, struct afp_comment * comment)
+	const char * pathname, struct afp_comment * comment)
 {
 	struct {
 		struct dsi_header dsi_header __attribute__((__packed__));
@@ -140,7 +141,7 @@ int afp_getcomment(struct afp_volume *volume, unsigned int did,
 	unixpath_to_afppath(volume->server,path);
 
 	rc=dsi_send(volume->server, (char *)msg,len,DSI_DEFAULT_TIMEOUT,
-		afpGetComment,comment);
+		afpGetComment,(void *) comment);
 	free(msg);
 	return rc;
 }
@@ -154,7 +155,7 @@ int afp_getcomment_reply(struct afp_server *server, char * buf, unsigned int siz
 	struct afp_comment * comment=other;
 	unsigned int len;
 	
-	if (size < sizeof (*reply_packet)) {
+	if (size < sizeof (struct dsi_header)) {
 		log_for_client(NULL,AFPFSD,LOG_WARNING,
 			"getcomment response is too short\n");
 		return -1;
@@ -166,6 +167,26 @@ int afp_getcomment_reply(struct afp_server *server, char * buf, unsigned int siz
 	comment->size=len;
 	return 0;
 }
+
+int afp_closedt(struct afp_server * server, unsigned short * refnum) 
+{
+	struct {
+		struct dsi_header dsi_header __attribute__((__packed__));
+		uint8_t command;
+		uint8_t pad;
+		uint16_t refnum ;
+	} __attribute__((__packed__)) request_packet;
+
+	dsi_setup_header(server,&request_packet.dsi_header,DSI_DSICommand);
+	request_packet.command=afpCloseDT;
+	request_packet.pad=0;  
+	request_packet.refnum=htons(refnum);
+
+	return dsi_send(server, (char *) &request_packet,
+		sizeof(request_packet),DSI_DEFAULT_TIMEOUT,afpCloseDT,NULL);
+}
+
+
 
 int afp_opendt(struct afp_volume *volume, unsigned short * refnum) 
 {
@@ -182,7 +203,8 @@ int afp_opendt(struct afp_volume *volume, unsigned short * refnum)
 	request_packet.volid=htons(volume->volid);
 
 	return dsi_send(volume->server, (char *) &request_packet,
-		sizeof(request_packet),DSI_DEFAULT_TIMEOUT,afpOpenDT,refnum);
+		sizeof(request_packet),DSI_DEFAULT_TIMEOUT,afpOpenDT,
+			(void *) refnum);
 }
 
 
