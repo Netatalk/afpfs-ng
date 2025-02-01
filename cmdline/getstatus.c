b4e1f1f66ef759db3ff9e0b788ec673ea14a6e01
Daniel Markstedt <daniel@mindani.net>
2025-02-02 00:07:42 +0100

    afpgetstatus: output IP addresses, protocol, signature

--- cmdline/getstatus.c
+++ cmdline/getstatus.c
@@ -5,61 +5,102 @@
 
 #include "afp.h"
 
-static int getstatus(char * address_string, unsigned int port)
+static int getstatus(char *address_string, unsigned int port)
 {
+	struct afp_server *server;
+	struct addrinfo hints, *res, *p;
+	int ret;
+	char signature_string[AFP_SIGNATURE_LEN * 2 + 1];
+	struct afp_versions *tmpversion;
+	char host[NI_MAXHOST], ipstr[INET6_ADDRSTRLEN];
 
-    struct afp_server *server;
-    struct hostent *h;
-    int ret;
-	struct addrinfo * address;
-	int j, firsttime=0;
-	char signature_string[AFP_SIGNATURE_LEN*2+1];
-	struct afp_versions * tmpversion;
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC; // Allow IPv4 or IPv6
+	hints.ai_socktype = SOCK_STREAM;
 
-    if ((address = afp_get_address(NULL,address_string, port)) == NULL) return -1;
+	char port_str[6];
+	snprintf(port_str, sizeof(port_str), "%u", port);
 
-    server=afp_server_init(address);
-
-    ret=afp_server_connect(server,1);
-
-    if (ret<0) {
-		perror("Connecting to server");
+	if ((ret = getaddrinfo(address_string, port_str, &hints, &res)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(ret));
 		return -1;
 	}
 
-    printf("Server name: %s\n",server->server_name_printable);
-	printf("Machine type: %s\n",server->machine_type);
+	if (res->ai_family == AF_INET) {
+		printf("Using IPv4\n");
+	} else if (res->ai_family == AF_INET6) {
+		printf("Using IPv6\n");
+	} else {
+		printf("Unknown address family\n");
+	}
+
+	server = afp_server_init(res);
+
+	ret = afp_server_connect(server, 1);
+
+	if (ret < 0) {
+		perror("Connecting to server");
+		freeaddrinfo(res);
+		return -1;
+	}
+
+	printf("Server name: %s\n", server->server_name_printable);
+	printf("Server type: %s\n", server->machine_type);
 	printf("AFP versions: \n");
 
-	for (j=0;j<SERVER_MAX_VERSIONS;j++) {
-		for (tmpversion=afp_versions;tmpversion->av_name;tmpversion++) {
-			if (tmpversion->av_number==server->versions[j]) {
-				printf("     %s\n",tmpversion->av_name);
+	for (int j = 0; j < SERVER_MAX_VERSIONS; j++) {
+		for (tmpversion = afp_versions; tmpversion->av_name; tmpversion++) {
+			if (tmpversion->av_number == server->versions[j]) {
+				printf("     %s\n", tmpversion->av_name);
 				break;
 			}
 		}
 	}
 
 	printf("UAMs:\n");
-	for (j=1;j<0x100;j<<=1) {
+	for (int j = 1; j < 0x100; j <<= 1) {
 		if (j & server->supported_uams) {
 			printf("     %s\n", uam_bitmap_to_string(j));
-			firsttime=1;
 		}
-	};
+	}
 
-	for (j=0;j<AFP_SIGNATURE_LEN;j++)
-		sprintf(signature_string+(j*2),"%02x",
-			(unsigned int) ((char) server->signature[j]));
+	printf("Signature:\n");
 
+		for (int j = 0; j < AFP_SIGNATURE_LEN; j++) {
+		printf("%02x ", (unsigned char)server->signature[j]);
+	}
+	printf(" ");
+	for (int j = 0; j < AFP_SIGNATURE_LEN; j++) {
+		unsigned char c = (unsigned char)server->signature[j];
+		if (c >= 32 && c <= 126) {
+			printf("%c", c);
+		} else {
+			printf(".");
+		}
+	}
+	printf("\n");
 
-	printf("Signature: %s\n", signature_string);
+    for (p = res; p != NULL; p = p->ai_next) {
+        void *addr;
+        char *ipver;
 
+        if (p->ai_family == AF_INET) {
+            struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
+            addr = &(ipv4->sin_addr);
+            ipver = "IPv4";
+        } else {
+            struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
+            addr = &(ipv6->sin6_addr);
+            ipver = "IPv6";
+        }
 
-	
-out:
-        free(server);
-        return 0;
+        inet_ntop(p->ai_family, addr, ipstr, sizeof(ipstr));
+        printf("Resolved %s address: %s\n", ipver, ipstr);
+    }
+
+	freeaddrinfo(res);
+	free(server);
+	return 0;
 }
 
 static void usage(void)
