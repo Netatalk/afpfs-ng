b32fcaec13c3015359a1b4d3524c858bfb63726e
Daniel Markstedt <daniel@mindani.net>
2025-02-07 08:05:33 +0100

    refactor away fileno() calls

--- fuse/fuse_error.c
+++ fuse/fuse_error.c
@@ -9,38 +9,41 @@
 
 #define TMP_FILE "/tmp/fuse_stderr"
 
-static int captured_fd;
-static fpos_t pos;
+static FILE* original_stderr = NULL;
 
-void report_fuse_errors(struct fuse_client * c)
+void report_fuse_errors(struct fuse_client* c) 
 {
 	char buf[1024];
-        int fd;
+	int fd;
 	int len;
-
+	
 	fflush(stderr);
-	dup2(captured_fd, fileno(stderr));
-	close(captured_fd);
-	clearerr(stderr);
-	fsetpos(stderr, &pos);        /* for C9X */
-
-        if ((fd=open(TMP_FILE,O_RDONLY))<0) return;;
-        memset(buf,0,1024);
-        len=read(fd,buf,1024);
-        close(fd);
-
+	stderr = original_stderr;
+	
+	if ((fd = open(TMP_FILE, O_RDONLY)) < 0) {
+		return;
+	}
+	
+	memset(buf, 0, 1024);
+	len = read(fd, buf, 1024);
+	close(fd);
 	unlink(TMP_FILE);
-
-	if (len>0) 
-		log_for_client((void *)c,AFPFSD,LOG_ERR,
-			"FUSE reported the following error:\n%s",buf);
+	
+	if (len > 0) {
+		log_for_client((void*)c, AFPFSD, LOG_ERR,
+			"FUSE reported the following error:\n%s", buf);
+	}
 }
 
 void fuse_capture_stderr_start(void)
 {
+	FILE* tmp_file;
+	
 	fflush(stderr);
-	fgetpos(stderr, &pos);
-	captured_fd = dup(fileno(stderr));
-	freopen(TMP_FILE, "a", stderr);
+	original_stderr = stderr;
+	
+	tmp_file = fopen(TMP_FILE, "a");
+	if (tmp_file) {
+		stderr = tmp_file;
+	}
 }
-
